e02520be45a32f590e0f993db37f18dc9a4c8c07
Evan Walter Clark Spotte-Smith, PhD <espottesmith@gmail.com>
2024-10-07 15:23:28 -0400

    Bringing up to date, including charge and spin table

--- src/radqm9_pipeline/processing/statistics.py
+++ src/radqm9_pipeline/processing/statistics.py
@@ -1,7 +1,9 @@
+import argparse
 import ast
 from glob import glob
 import logging
 import multiprocessing as mp
+import os
 from typing import List, Tuple
 
 import tqdm
@@ -11,14 +13,125 @@ import numpy as np
 import torch_geometric
 
 from mace import tools, data
-from mace.tools.utils import AtomicNumberTable
+from mace.tools.utils import AtomicNumberTable, TotalChargeTable, SpinTable
 from mace.modules import compute_statistics
 
-from radqm9_pipeline.processing.data_arg_parser import build_default_arg_parser
+def build_arg_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument(
+        "--prefix",
+        help="The prefix for the h5 file.",
+        type=str,
+        required = True
+    )
+    parser.add_argument(
+        "--atomic_numbers",
+        help="List of atomic numbers",
+        type=str,
+        default=None,
+        required=False,
+    )
+    parser.add_argument(
+        "--total_charges",
+        help="List of charges",
+        type=str,
+        default=None,
+        required=False,
+    )
+    parser.add_argument(
+        "--spins",
+        help="List of spin multiplicities",
+        type=str,
+        default=None,
+        required=False,
+    )
+    parser.add_argument(
+        "--num_process",
+        help="The user defined number of processes to use(the number of files created).", 
+        type=int, 
+        default=int(os.cpu_count()/4)
+    )
+    parser.add_argument(
+        "--E0s",
+        help="Dictionary of isolated atom energies",
+        type=str,
+        default=None,
+        required=False,
+    )
+    parser.add_argument(
+        "--r_max",
+        help="distance cutoff (in Ang)", 
+        type=float, 
+        default=5.0
+    )
+    parser.add_argument(
+        "--batch_size", 
+        help="batch size", 
+        type=int, 
+        default=10)
+    parser.add_argument(
+        "--config_type_weights",
+        help="String of dictionary containing the weights for each config type",
+        type=str,
+        default='{"Default":1.0}',
+    )
+    parser.add_argument(
+        "--energy_key",
+        help="Key of reference energies in training xyz",
+        type=str,
+        default="energy",
+    )
+    parser.add_argument(
+        "--forces_key",
+        help="Key of reference forces in training xyz",
+        type=str,
+        default="forces",
+    )
+    parser.add_argument(
+        "--virials_key",
+        help="Key of reference virials in training xyz",
+        type=str,
+        default="virials",
+    )
+    parser.add_argument(
+        "--stress_key",
+        help="Key of reference stress in training xyz",
+        type=str,
+        default="stress",
+    )
+    parser.add_argument(
+        "--dipole_key",
+        help="Key of reference dipoles in training xyz",
+        type=str,
+        default="dipole",
+    )
+    parser.add_argument(
+        "--charges_key",
+        help="Key of atomic charges in training xyz",
+        type=str,
+        default="charges",
+    )
+   
+    return parser
 
 
-def compute_stats_target(file: str, z_table: AtomicNumberTable, r_max: float, atomic_energies: Tuple, batch_size: int):
-    train_dataset = data.HDF5Dataset(file, z_table=z_table, r_max=r_max)
+
+def compute_stats_target(
+    file: str,
+    z_table: AtomicNumberTable,
+    r_max: float,
+    atomic_energies: Tuple,
+    batch_size: int,
+    total_charge_table: TotalChargeTable,
+    spin_table: SpinTable):
+    train_dataset = data.HDF5Dataset(
+        file,
+        z_table=z_table,
+        r_max=r_max,
+        total_charge_table=total_charge_table,
+        spin_table=spin_table
+    )
     train_loader = torch_geometric.dataloader.DataLoader(
         dataset=train_dataset, 
         batch_size=batch_size, 
@@ -32,10 +145,10 @@ def compute_stats_target(file: str, z_table: AtomicNumberTable, r_max: float, at
 
 
 def pool_compute_stats(inputs: List): 
-    path_to_files, z_table, r_max, atomic_energies, batch_size, num_process = inputs
+    path_to_files, z_table, r_max, atomic_energies, batch_size, total_charges, spins, num_process = inputs
     pool = mp.Pool(processes=num_process)
     
-    re=[pool.apply_async(compute_stats_target, args=(file, z_table, r_max, atomic_energies, batch_size,)) for file in glob(path_to_files+'/*')]
+    re=[pool.apply_async(compute_stats_target, args=(file, z_table, r_max, atomic_energies, batch_size, total_charges, spins)) for file in glob(path_to_files+'/*')]
     
     pool.close()
     pool.join()
@@ -43,8 +156,10 @@ def pool_compute_stats(inputs: List):
     return np.average(results, axis=0)
 
 
+#TODO: TypeError: HDF5Dataset.__init__() missing 2 required positional arguments: 'total_charge_table' and 'spin_table'
+
 if __name__ == "__main__":
-    args = build_default_arg_parser().parse_args()
+    args = build_arg_parser().parse_args()
 
     try:
         config_type_weights = ast.literal_eval(args.config_type_weights)
@@ -67,7 +182,7 @@ if __name__ == "__main__":
     atomic_energies: np.ndarray = np.array(
         [0.0 for z in z_table.zs]
     )
-    _inputs = [args.prefix, z_table, args.r_max, atomic_energies, args.batch_size, args.num_process]
+    _inputs = [args.prefix, z_table, args.r_max, atomic_energies, args.batch_size, args.total_charges, args.spins, args.num_process]
     avg_num_neighbors, mean, std = pool_compute_stats(_inputs)
     logging.info(f"Average number of neighbors: {avg_num_neighbors}")
     logging.info(f"Mean: {mean}")
